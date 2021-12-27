1984f4517fa7314035f9e729546bb8a968f17f99
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-27 15:13:58 +0200

    day 01

--- src/01.clj
+++ src/01.clj
@@ -1,6 +1,16 @@
 (ns japiirainen.aoc-2015.01
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [trim]]))
 
-(comment
-  (println "foo"))
+(def input (->> (slurp "inputs/01") trim))
 
+(defn find-basement [input]
+  (letfn [(go [n [c & rest]]
+              (if (= n -1)
+                (- (count input) (inc (count rest)))
+                (case c
+                  \( (recur (inc n) rest)
+                  \) (recur (dec n) rest))))]
+    (go 0 input)))
+
+(->> input frequencies vals (apply -) (println "Part 1:"))
+(->> input find-basement (println "Part 2:"))
