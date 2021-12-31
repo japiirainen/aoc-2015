3b92cc7ebb6d90a63706ef91e30ee718190d77ed
Joona Piirainen <joona.piirainen@gmail.com>
2021-12-31 11:43:55 +0200

    day 11 and 12

---
+++ src/11.clj
@@ -0,0 +1,34 @@
+(ns japiirainen.aoc-2015.11
+  (:require [clojure.string :as str]))
+
+(defn next-pw [pw]
+  (case (last pw)
+    \z (str (->> pw drop-last next-pw) \a)
+    ; skip i o l
+    \h (str (->> pw drop-last (str/join "")) \j)
+    \n (str (->> pw drop-last (str/join "")) \p)
+    \k (str (->> pw drop-last (str/join "")) \m)
+    (str (->> pw drop-last (str/join "")) (->> pw last int inc char))))
+
+(defn increasing-triple? [triple]
+  (let [[a b c] (map int triple)]
+    (= (inc a) b (dec c))))
+
+(defn has-two-pairs? [[[a b] & pairs]]
+  (and
+   a
+   (or
+    (and (= a b) (some (fn [[c d]] (and (= c d) (not= a c))) (rest pairs)))
+    (recur pairs))))
+
+(defn valid-pw [pw]
+  (and
+   (->> pw (partition 3 1) (some increasing-triple?))
+   (->> pw (partition 2 1) has-two-pairs?)))
+
+(defn find-valid [start]
+  (->> start (iterate next-pw) rest (filter valid-pw) first))
+
+(let [[_ p1 p2] (->> (slurp "inputs/11.txt") (iterate find-valid) (take 3))]
+  (prn "Part 1" p1)
+  (prn "Part 2" p2))
---
+++ src/12.clj
@@ -0,0 +1,24 @@
+(ns japiirainen.aoc-2015.12
+  (:require [cheshire.core :as cc]))
+
+(def JSON (->> "inputs/12.txt" slurp cc/parse-string))
+
+(defn sum-json-1 [o]
+  (cond
+    (map? o) (->> o vals (map sum-json-1) (reduce +))
+    (vector? o) (->> o (map sum-json-1) (reduce +))
+    (integer? o) o
+    :else 0))
+
+(defn sum-json-2 [o]
+  (cond
+    (map? o)
+    (if (->> o vals (some #(= "red" %)))
+      0
+      (->> o vals (map sum-json-2) (reduce +)))
+    (vector? o) (->> o (map sum-json-2) (reduce +))
+    (integer? o) o
+    :else 0))
+
+(->> JSON sum-json-1 (prn "Part 1:"))
+(->> JSON sum-json-2 (prn "Part 2:"))
